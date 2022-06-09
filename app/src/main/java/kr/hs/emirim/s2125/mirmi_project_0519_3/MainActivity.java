ac719e0a234396a60257e22288c162a1df9aefaa
seungchae1 <s2125@e-mirim.hs.kr>
2022-06-09 19:08:29 +0900

    활용

--- app/src/main/java/kr/hs/emirim/s2125/mirmi_project_0519_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2125/mirmi_project_0519_3/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Chronometer;
 import android.widget.DatePicker;
+import android.widget.FrameLayout;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     TimePicker time;
     DatePicker date;
     TextView textResult;
+    FrameLayout frame;
     int selectedYear, selectedMonth, selectedDay;
 
     @Override
@@ -32,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         time = findViewById(R.id.time);
         date = findViewById(R.id.date);
         textResult = findViewById(R.id.text_result);
+        frame=findViewById(R.id.frame);
         rg.setOnCheckedChangeListener(rgListener);
         timer.setOnClickListener(timerListener);
         textResult.setOnLongClickListener(textListener);
@@ -43,12 +46,14 @@ public class MainActivity extends AppCompatActivity {
                 selectedDay = day;
             }
         });
-        time.setVisibility(View.INVISIBLE);
-        date.setVisibility(View.INVISIBLE);
+        rg.setVisibility(View.INVISIBLE);
+        frame.setVisibility(View.INVISIBLE);
     }
+
     RadioGroup.OnCheckedChangeListener rgListener = new RadioGroup.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+            frame.setVisibility(View.VISIBLE);
             time.setVisibility(View.INVISIBLE);
             date.setVisibility(View.INVISIBLE);
             switch(checkedId){
@@ -65,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener timerListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            rg.setVisibility(View.VISIBLE);
             timer.setBase(SystemClock.elapsedRealtime());
             timer.start();
             timer.setTextColor(Color.RED);
@@ -78,6 +84,8 @@ public class MainActivity extends AppCompatActivity {
             timer.setTextColor(Color.BLUE);
             textResult.setText(selectedYear+"년 "+selectedMonth+"월 "+selectedDay+"일");
             textResult.append(time.getCurrentHour()+"시 "+time.getCurrentMinute()+"분 예약 완료됨");
+            frame.setVisibility(View.INVISIBLE);
+            rg.setVisibility(View.INVISIBLE);
             return true;
         }
     };
